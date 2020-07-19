ff4aa6b28522cc2427d45150ef208c900fa970cb
CAMERON LUYT <cameronluyt30@gmail.com>
2020-07-20 00:52:07 +0200

    .

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default {
+  products: [
+      {
+      id:'1',
+      name: "HUAWEI P40 Lite (Dual SIM) - Black",
+      category: "Phones",
+      image: "https://www.computermania.co.za/pub/media/catalog/product/cache/10f519365b01716ddb90abc57de5a837/4/5/453535.jpg",
+      price: 6999.00,
+      brand:"HUAWEI",
+      rating: 4.5,
+      numberReviews: 10,
+      countInStock: 6
+      },
+      {
+      id:'2',
+      name: "Asus Zenbook Duo 14 - UX481FL-BM043T",
+      category: "Laptop",
+      image: "https://www.computermania.co.za/pub/media/catalog/product/cache/10f519365b01716ddb90abc57de5a837/u/x/ux481fl_1.jpg",
+      price: 29999.00,
+      brand:"ASUS",
+      rating: 4.2,
+      numberReviews: 22,
+      countInStock: 56
+      },
+      {
+      id:'3',
+      name: "Samsung Galaxy A51 (Dual SIM) Blue",
+      category: "Phone",
+      image: "https://www.computermania.co.za/pub/media/catalog/product/cache/10f519365b01716ddb90abc57de5a837/7/1/71t5oy5tbhl._ac_sl1500_.jpg",
+      price: 7499.00,
+      brand:"Samsung",
+      rating: 4.8,
+      numberReviews: 15,
+      countInStock: 9
+      }
+  ]
+};
